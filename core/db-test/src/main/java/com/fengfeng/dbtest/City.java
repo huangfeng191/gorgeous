8c7774cafac7501da49b9eec26cad58d3d036a86
fengfeng <huangfeng191@163.com>
2021-02-23 01:11:32 +0800

    a

---
+++ core/db-test/src/main/java/com/fengfeng/dbtest/City.java
@@ -0,0 +1,8 @@
+package com.fengfeng.dbtest;
+
+import lombok.Data;
+
+@Data
+public class City {
+    private String state;
+}
--- core/db-test/src/main/java/com/fengfeng/dbtest/StartApplication.java
+++ core/db-test/src/main/java/com/fengfeng/dbtest/StartApplication.java
@@ -1,14 +1,25 @@
 package com.fengfeng.dbtest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StartApplication {
+public class StartApplication implements CommandLineRunner {
+
+	private final CityMapper cityMapper;
+
+	public StartApplication(CityMapper cityMapper) {
+		this.cityMapper = cityMapper;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartApplication.class, args);
 		System.out.println("So easy ");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.cityMapper.findByState("CA"));
+	}
 }
