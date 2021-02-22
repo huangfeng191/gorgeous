fabad2b73dacdf3c132d673cfc8c9ab08393118e
fengfeng <huangfeng191@163.com>
2021-02-22 17:49:08 +0800

    a

--- core/quartz/src/main/java/com/fengfeng/quartz/controller/QuartzController.java
+++ core/quartz/src/main/java/com/fengfeng/quartz/controller/QuartzController.java
@@ -4,12 +4,14 @@ import com.fengfeng.quartz.job.quartz.QuartzManager;
 import com.fengfeng.quartz.job.quartz.WebJob;
 
 
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+
 
 @RestController
 @RequestMapping("/quartz")
